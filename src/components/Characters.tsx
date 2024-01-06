299221107f562641e7e7905c85702809fb7421b7
Dhruv Barad <barad019@umn.edu>
2024-01-06 16:35:46 -0600

    Some refactors.

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -11,23 +11,25 @@ interface CharactersProps {
 const Characters = ({array, header, characterType}: CharactersProps) => {
     const rowRef: RefObject<HTMLDivElement> = useRef(null);
     return (array ? (
-        <div className={`row ${header === "" ? "" : "mt-5"} mb-5 animate__animated animate__fadeIn ${characterType}`}>
-            <h3 className="text-center">{header}</h3>
-            <div className="col text-center d-flex justify-content-end align-items-center">
-                <button className="btn float-end"
-                        onClick={() => scrollToPreviousCard(rowRef, 4)}>&lt;</button>
-            </div>
-            <div className="col-10">
-                <div className="row flex-nowrap overflow-auto" ref={rowRef}>
-                    {array.map((item, index) => (
-                        <Character key={index} copyRightHTML={item.copyRightHTML} id={item.id}
-                                   name={item.name} description={item.description}
-                                   imageSource={item.imageSource} type={item.type}/>
-                    ))}
+        <div className={`container-fluid ${characterType}`}>
+            <div className={`row ${header === "" ? "" : "mt-5"} mb-5 animate__animated animate__fadeIn`}>
+                <h3 className="text-center">{header}</h3>
+                <div className="col text-center d-flex justify-content-end align-items-center">
+                    <button className="btn float-end"
+                            onClick={() => scrollToPreviousCard(rowRef, 4)}>&lt;</button>
                 </div>
-            </div>
-            <div className="col text-center d-flex justify-content-start align-items-center">
-                <button className="btn" onClick={() => scrollToNextCard(rowRef, 4)}>&gt;</button>
+                <div className="col-10">
+                    <div className="row flex-nowrap overflow-auto" ref={rowRef}>
+                        {array.map((item, index) => (
+                            <Character key={index} copyRightHTML={item.copyRightHTML} id={item.id}
+                                       name={item.name} description={item.description}
+                                       imageSource={item.imageSource} type={item.type}/>
+                        ))}
+                    </div>
+                </div>
+                <div className="col text-center d-flex justify-content-start align-items-center">
+                    <button className="btn" onClick={() => scrollToNextCard(rowRef, 4)}>&gt;</button>
+                </div>
             </div>
         </div>
     ) : (
